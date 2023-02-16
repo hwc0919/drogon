f6c5c84095effc6b12e2c8cb6605191e96430441
He, Wanchen <hwc14@qq.com>
2023-02-16 16:41:23 +0800

    Add except-free feature to helloworld.

--- examples/helloworld/HelloController.cc
+++ examples/helloworld/HelloController.cc
@@ -12,9 +12,12 @@ class SayHello : public HttpController<SayHello>
     // a handler to /. But because it is a part of the SayHello controller. It
     // ends up in path /SayHello/ (IMPORTANT! It is /SayHello/ not /SayHello
     // as they are different paths).
-    METHOD_ADD(SayHello::genericHello, "/", Get);
+    METHOD_ADD(SayHello::genericHello, "/", Get, HandlerFeature::ExceptFree);
     // Same for /hello. It ends up at /SayHello/hello
-    METHOD_ADD(SayHello::personalizedHello, "/hello", Get);
+    METHOD_ADD(SayHello::personalizedHello,
+               "/hello",
+               Get,
+               HandlerFeature::ExceptFree);
     METHOD_LIST_END
 
   protected:
